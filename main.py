edbbe8cdc6360cd0994222d2a664ffc791a46721
ehsan tafehi <ehsan.tafehi@dnb.no>
2025-09-05 16:44:16 +0200

    adding elevenlabs for text/speech/text mcp

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 ## requirements
 
 ## MCP for speach like whisper in openai
-## MCP for Text to speech
+## MCP for Text to speech and vice versa https://github.com/elevenlabs/elevenlabs-mcp
+## https://elevenlabs.io/
 ## Vision API
 ## LLM
 ## https://github.com/svpino/alloy-voice-assistant.git
